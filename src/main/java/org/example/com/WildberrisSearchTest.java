b152233f8fba3090046e3d3d22fdff3d8419090c
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-04-14 15:38:58 +0400

    feat: added writing in file and added README.md

--- src/main/java/org/example/com/WildberrisSearchTest.java
+++ src/main/java/org/example/com/WildberrisSearchTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.*;
 import java.time.Duration;
 import java.util.List;
 
@@ -19,6 +20,8 @@ public class WildberrisSearchTest {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+
             driver.get("https://www.wildberries.ru/");
             Thread.sleep(2000);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -27,7 +30,7 @@ public class WildberrisSearchTest {
 
             driver.findElement(By.cssSelector(".dropdown-filter__btn--sorter")).click();
             driver.findElement(By.xpath("//span[@class='radio-with-text__text' and text()='По возрастанию цены']")).click();
-            Thread.sleep(300);
+            Thread.sleep(1000);
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".product-card-list")));
             List<WebElement> products = driver.findElements(By.cssSelector(".product-card.product-card--hoverable.j-card-item"));
 
@@ -35,10 +38,16 @@ public class WildberrisSearchTest {
                 WebElement product = products.get(i);
                 String name = product.findElement(By.cssSelector("[aria-label]"))
                         .getAttribute("aria-label");
-                String price = product.findElement(By.cssSelector("ins.price__lower-price.wallet-price")).getText();
-                System.out.println((i + 1) + ". " + name + " - Цена: " + price);
+                String price = product.findElement(By.cssSelector("ins.price__lower-price")).getText();
+                String line = (i + 1) + ". " + name + " - Цена: " + price;
+                writer.write(line);
+                writer.newLine();
+                System.out.println(line);
             }
+            writer.close();
         } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
             driver.quit();
